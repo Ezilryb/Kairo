951b0448ab2fd961f7da79e25172ed556dca668f
Ezilryb <ezilrybtrash@gmail.com>
2026-08-30 14:54:07 +0200

    fix: migrate to ESLint flat config, regenerate cross-platform lockfile

---
+++ eslint.config.mjs
@@ -0,0 +1,8 @@
+// /eslint.config.mjs
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
+
+export default [
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+];
